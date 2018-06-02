9a6fdbf2dcec5eba192d84323bc601d1d9aaaa31
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-06-02 18:23:59 -0300

    potter: Include MotoCamera instead of Gcam

--- device.mk
+++ device.mk
@@ -120,7 +120,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    GoogleCameraMod \
+    com.motorola.cameraone \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
